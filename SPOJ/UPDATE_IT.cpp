baa2b3c07d3eba58e723770edf5d264796e39c3d
varadkulkarni11 <varadkulkarnirules@gmail.com>
2017-09-26 01:24:21 +0530

    Update UPDATE_IT.cpp

--- SPOJ/UPDATE_IT.cpp
+++ SPOJ/UPDATE_IT.cpp
@@ -11,7 +11,7 @@ typedef long long ll;
 typedef unsigned long long ull;
 bool pr[1000007];
 void sieve(){pr[0]=1;pr[1]=1;for(int i=2;i<sqrt(1000007);i++){for(int j=2*i;j<=1000007;j+=i){pr[j]=1;}}}
-ll fashtpower(ll x,ll y){ll res=1;while(y){if(y&1)res=res*x;res%=MOD;y=y>>1;x=x*x;x%=MOD;}return res;}
+ll fastpower(ll x,ll y){ll res=1;while(y){if(y&1)res=res*x;res%=MOD;y=y>>1;x=x*x;x%=MOD;}return res;}
 ull lopow(ll x,ll y){ull res=1ULL;while(y){if(y&1)res*=(ull)x;y>>=1;x*=x;}return res;}
 ll a[100003];
 ll sta[100003]; //hash for start points
